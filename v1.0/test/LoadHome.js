4bcbbc27c668fada710d13a64e6f64c5f5781162
Prasadi Warnaweera <p.waranaweera@gmail.com>
2017-03-25 19:27:41 -0500

    Delete LoadHome.js

--- v1.0/test/LoadHome.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	'Load Imgur Home Test': function (browser) {
-		browser
-			.click('a[href="//imgur.com"]')
-			.waitForElementVisible('body', 1000)
-			.assert.title('Imgur: The most awesome images on the Internet')
-	}
-};
