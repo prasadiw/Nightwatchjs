9747bfc0012dddcda9f1d8244f1f7e1753e39095
Prasadi Warnaweera <p.waranaweera@gmail.com>
2017-03-27 00:13:20 -0500

    Updated the test for uploading an image.

--- test/uploadImage.js
+++ test/uploadImage.js
@@ -15,7 +15,7 @@ module.exports = {
 			.waitForElementVisible('#upload-modal', 1000)//waits for the image upload control to be loaded
 			.waitForElementVisible('.browse-btn', 1000)
 			//.waitForElementVisible('#global-files-button', 1000)
-			.setValue('#global-files-button', require('path').resolve(__dirname + '/images/images.jpg'))//upload the specified image
+			.setValue('#global-files-button', require('path').resolve(__dirname + '/images/image1.jpg'))//upload the specified image
 			//assert the new post title header
 			.waitForElementVisible('#upload-global > div > span:nth-child(3) > div > div.left.post-pad > div > div.post-header.is-owner > div > div.post-title-container > h1', 10000)
 			//assert the image holder
