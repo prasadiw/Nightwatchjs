a34239710155708eb5e22909084296f8758dd6dd
Prasadi Warnaweera <p.waranaweera@gmail.com>
2017-03-27 13:58:12 -0500

    Increase wait time for elements in firefox
    Refine wait times to minimize test fails which happens due to delay of element loading in firefox.

--- v2.0/FireFoxSpecificTests/autoCompleteSearch.js
+++ v2.0/FireFoxSpecificTests/autoCompleteSearch.js
@@ -26,14 +26,14 @@ module.exports = {
 	'Test Auto Complete Search with TAGS': function (browser) {
 		browser
 			.waitForElementVisible('body', 1000)
-			.waitForElementVisible('#global-search-container', 1000)
+			.waitForElementVisible('#global-search-container', 5000)
 			.click('#global-search-container > div.search-container.search-focused > form.search-form > div.search-icon-container > div.icon-search')//click on search control
 			//clear the current search text
 			.clearValue('#global-search-container > div.search-container.search-focused > form > input')
 			//set the search text
 			.setValue('#global-search-container > div.search-container.search-focused > form > input', searchtext)
 			//wait for global search auto complete panel
-			.waitForElementVisible('#global-search-container > div.search-container.search-focused > div.global-search-autocomplete', 5000)
+			.waitForElementVisible('#global-search-container > div.search-container.search-focused > div.global-search-autocomplete', 10000)
 			.pause(2000)
 			//If Tag suggestions are found, search by tags
 			.element('css selector', '#global-search-container > div.search-container.search-focused > div.global-search-autocomplete > div:nth-child(1) > div.tag-icon', function (result) {
@@ -52,7 +52,7 @@ module.exports = {
 				}
 				//Navigate to home page
 				browser.click('a[href="//imgur.com"]')
-				browser.waitForElementVisible('body', 1000)
+				browser.waitForElementVisible('body', 5000)
 				browser.assert.title('Imgur: The most awesome images on the Internet')
 
 			});
@@ -63,9 +63,10 @@ module.exports = {
 	'Test Auto Complete Search with USERS': function (browser) {
 		browser
 			.waitForElementVisible('body', 5000)
-			.waitForElementVisible('#global-search-container', 1000)
-			.click('#global-search-container > div.search-container.search-closed > form.search-form > div.search-icon-container > div.icon-search.searchable')//click on search control
+			.waitForElementVisible('#global-search-container', 5000)
+			.click('#global-search-container > div.search-container.search-closed > form.search-form > div.search-icon-container > div.icon-search')//click on search control
 			//set the search text
+			.pause(2000)
 			.setValue('#global-search-container > div.search-container.search-focused > form > input', searchtext)
 			//wait for global search auto complete panel
 			.waitForElementVisible('#global-search-container > div.search-container.search-focused > div.global-search-autocomplete', 5000)
@@ -75,13 +76,13 @@ module.exports = {
 				if (result.status != -1) { //wait for auto suggestions using Users
 					//click on first user result
 					browser.click('#global-search-container > div.search-container.search-focused > div.global-search-autocomplete > div:nth-child(2) > a:nth-child(3)')
-					browser.waitForElementVisible('body', 1000)
+					browser.waitForElementVisible('body', 5000)
 					browser.pause(2000)
 					browser.assert.urlContains('imgur.com/user/')//assert page url for user page
 					browser.waitForElementVisible('#captions > div.panel-header.textbox', 1000)//wait for header					
 					browser.element('css selector', '#captions > div.captions-container.infinite-scroll-loaded', function (result) {
 						if (result.status != -1) { //If user comments results are found, locate the first user comment
-							browser.waitForElementVisible('#captions > div.captions-container.infinite-scroll-loaded > div > div > div:nth-child(1) > div.comment-wrapper', 3000)
+							browser.waitForElementVisible('#captions > div.captions-container.infinite-scroll-loaded > div > div > div:nth-child(1) > div.comment-wrapper', 5000)
 							//verify username for the search text
 							browser.assert.containsText('#captions > div.captions-container.infinite-scroll-loaded > div > div > div:nth-child(1) > div.comment-wrapper > div.comment > div > div.usertext.cf > div.author > a.comment-username', searchtext)
 							browser.end();
